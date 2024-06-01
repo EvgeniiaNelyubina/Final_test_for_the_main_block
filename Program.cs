4b35138b9e1ed2055e083b9012ceee3d8d9a8e38
Nelyubina Evgeniia <klimova.eug@yandex.ru>
2024-06-01 23:37:03 +0300

    Подсчет подходящих строк

--- Program.cs
+++ Program.cs
@@ -2,3 +2,17 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+using System;
+
+class Program
+{
+    static string[] FilterStrings(string[] array)
+    {
+        int count = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
